b8f8dfb96b67f6905971d2080fc0bcb289965e13
Joe <rxyjoey@gmail.com>
2020-10-20 15:57:18 +0800

    修改baseURL

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,9 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://sh.asdk.io:8888/jksj',
+  // oa.zjjksjy.com
+  baseURL: '/api',
+  // baseURL: 'http://sh.asdk.io:8888/jksj',
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
